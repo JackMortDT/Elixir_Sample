efcad942c601e62a207f191429ffa93f70068d41
LuisSas <luis@makingdevs.com>
2018-09-17 19:17:19 -0500

    Adding ecercise to known what is your first and your last name

---
+++ basicexercises/user.exs
@@ -0,0 +1,46 @@
+defmodule User do
+  @moduledoc """
+  Define the user struct and functions to handle users.
+  """
+  import String, only: [split: 1]
+  import List, only: [first: 1, last: 1]
+
+  defstruct name: nil, email: nil
+
+  @doc """
+  Get the first name of a user.
+
+  ## Parameters
+
+  - `user` - A User struct.
+
+  ## Examples
+
+      user = %User{name: "Luis Sastré"}
+      User.first_name(user)
+      "Luis"
+  """
+  def first_name(user) do
+    user.name
+    |> split
+    |> first
+  end
+  @doc """
+  Get the last name of a user.
+
+  ## Parameters
+
+  - `user` - A User struct.
+
+  ## Examples
+
+      user = %User{name: "Luis Sastré"}
+      User.last_name(user)
+      "Sastré"
+  """
+  def last_name(user) do
+    user.name
+    |> split
+    |> last
+  end
+end
